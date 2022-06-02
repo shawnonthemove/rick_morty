f81d9d9886b9f26db84c407c7e6509ca1828e2c2
肖宝楠（实习） <xiaobaonan@xiaohongshu.com>
2022-06-02 17:28:00 +0800

    feat: 增加文件

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import React, {useState, useEffect} from 'react';
+import Search from "./components/Search/Search";
+import Card from "./components/Card/Card";
+import Pagination from "./components/Pagination/Pagination";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
+
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  let [fetchedData, setFetchedData] = useState([]);
+  let [ info, setInfo ] = useState({})
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res => res.json())
+      setInfo(data.info)
+      setFetchedData(data.results)
+    })()
+  }, [api])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-center mb-3'>Characters</h1>
+      <Search setPageNumber={setPageNumber} setSearch={setSearch}></Search>
+      <div className='container'>
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={fetchedData}></Card>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Pagination
+      info = {info}
+      pageNumber = {pageNumber}
+      setPageNumber = {setPageNumber}
+      ></Pagination>
     </div>
   );
 }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Search.module.scss';
+
+export default function Search({ setPageNumber, setSearch }) {
+    let searchBtn = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <form className={`${styles.search} d-flex flex-sm-row flex-column align-items-center justify-content-center gap-4 mb-5`}>
+            <input onChange={(e) => {
+                setSearch(e.target.value)
+                setPageNumber(1)
+            }} placeholder="Search for characters"
+            className={styles.input}
+            type="text"
+            />
+            <button
+            onClick={searchBtn}
+            className={`${styles.btn} btn btn-primary fs-5`}
+            >
+            Search
+            </button>
+        </form>
+    )
+}
